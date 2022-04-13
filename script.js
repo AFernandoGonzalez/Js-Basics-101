fe0f87a7a042e8e9202d53af3fbbd90c47157aa1
Fernando <ferg.developer@gmail.com>
2022-04-12 21:54:21 -0400

    Variables

---
+++ script.js
@@ -0,0 +1,34 @@
+// document.write('Hello World');
+// alert('Hello World');
+// console.log('Hello World);
+
+// document.write('One');
+// alert('Two');
+// console.log('Three');
+
+// document.write({name: 'Fer'});
+// alert({name: 'Fer'});
+// console.log({name: 'Fer'});
+
+
+// ------------- Statements ----------------//
+// I woke up early today.
+// Statements - sets of instructions
+// comments - shortcut Commands +/
+
+// console.log('Hi');
+
+
+// ------------- Variables ----------------//
+
+let fName = 'hey';
+let address, zip, state;
+address = '35 St';
+zip = 123;
+state = 'NY'
+console.log(fName);
+console.log(address);
+console.log(zip);
+console.log(state);
+console.log(address, zip, state);
+// some code
